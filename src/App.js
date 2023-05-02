8ff869a22da896429a24dc664995a2bdf5789ed5
Rahul Gehlot <rahulgehlot9004@gmail.com>
2023-05-02 12:02:53 +0530

    Adding more Functionalities

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 
@@ -8,6 +9,7 @@ function App() {
       <Navbar title="TextUtils" about="About Us" />
       <div className="container my-3">
         <TextForm heading = "Enter the text to analyze below" />
+        {/* <About /> */}
       </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 export default function TextForm(props) {
   const handleUpClick = () => {
@@ -7,34 +7,65 @@ export default function TextForm(props) {
     setText(newText);
   };
 
+  const handleLoClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
+
+  const handleCopyClick = () => {
+    navigator.clipboard.writeText(text);
+    setText("Text Copied");
+  };
+
   const handleOnChange = (event) => {
     // console.log("On change");
     setText(event.target.value);
   };
 
   // state variable
-  const [text, setText] = useState("Enter text here");
+  const [text, setText] = useState("");
   // wrong way to change the state
   //    text = "new text";
 
   //   correct way to change the state
   // setText("New text");
   return (
-    <div>
-      <h1>{props.heading}</h1>
-      <div className="mb-3">
-        <textarea
-          className="form-control"
-          value={text}
-          onChange={handleOnChange}
-          id="mybox"
-          rows="8"
-        ></textarea>
+    <>
+      <div className="container">
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            id="mybox"
+            rows="8"
+          ></textarea>
 
-        <button className="btn btn-primary my-3" onClick={handleUpClick}>
-          Convert to Uppercase
-        </button>
+          <button className="btn btn-primary my-3 mx-1" onClick={handleUpClick}>
+            Convert to Uppercase
+          </button>
+          <button className="btn btn-primary my-3 mx-1" onClick={handleLoClick}>
+            Convert to LowerCase
+          </button>
+          <button
+            className="btn btn-primary my-3 mx-1"
+            onClick={handleCopyClick}
+          >
+            Copy to Clipboard
+          </button>
+        </div>
       </div>
-    </div>
+      <div className="container my - 3 ">
+        <h2>Your Text summery</h2>
+        <p>
+          {" "}
+          {text.split(" ").length} words and {text.length} characters
+        </p>
+        <p>{0.008 * text.split(" ").length} Minutes read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+      </div>
+    </>
   );
 }
